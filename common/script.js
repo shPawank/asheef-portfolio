127815fc9a89eaf64146d052575e8e91088293ae
Pawan Kumar Sharma <120400317+shPawank@users.noreply.github.com>
2025-04-10 12:33:03 +0000

    Initial commit for Asheef portfolio

--- common/script.js
+++ common/script.js
@@ -1,13 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
-    fetch("Common/header.html")
+    fetch("common/header.html")
       .then(res => res.text())
       .then(data => document.getElementById("header").innerHTML = data);
   
-    fetch("Common/footer.html")
+    fetch("common/footer.html")
       .then(res => res.text())
       .then(data => document.getElementById("footer").innerHTML = data);
   });
-  fetch("Common/header.html")
+  fetch("common/header.html")
   .then(response => {
     if (!response.ok) throw new Error("Failed to load header");
     return response.text();
